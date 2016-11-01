07029026b29a7693da755f9c80d73db10e16ef74
Kittylekat <kitty@ionpowered.info>
2016-11-01 02:29:07 +0100

    added db:seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+task1 = Task.create(
+    title: 'Example Task 1',
+    text: 'Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum',
+    created_at: Date.today-10,
+    updated_at: Date.today-5)
+task2 = Task.create(
+    title: 'Example Task 2',
+    text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. ',
+    created_at: Date.today-4,
+    updated_at: Date.today-3)
+
+Comment.create(
+    task_id: task1.id,
+    content: 'Wow amazing!',
+    created_at: Date.today-6,
+    updated_at: Date.today-3)
+Comment.create(
+    task_id: task1.id,
+    content: 'Mah',
+    created_at: Date.today-2,
+    updated_at: Date.today-2)
+Comment.create(
+    task_id: task2.id,
+    content: 'First lol',
+    created_at: Date.today-4,
+    updated_at: Date.today-3)
+Comment.create(
+    task_id: task2.id,
+    content: 'Hope you write more',
+    created_at: Date.today-2,
+    updated_at: Date.today-1)
+Comment.create(
+    task_id: task2.id,
+    content: 'Nice one!',
+    created_at: Date.today-1,
+    updated_at: Date.today-1)
