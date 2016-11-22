88ec7195e5e5cc98cbb5b682623906cab14adbae
Kittylekat <kitty@ionpowered.info>
2016-11-22 01:05:48 +0100

    Fixed some errors

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,29 +19,34 @@ class TasksController < ApplicationController
 
 	def create
 		@task = Task.new(task_params)
+    #@tasks = Task.all
 
 		respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
-        format.js   {}
-        format.json { render :show, status: :created, location: @task }
+        format.html { redirect_to tasks_path, notice: 'Task was successfully created.' }
+        #format.js   {}
+        format.json { render :show, status: :created, location: @tasks }
+        #format.json { render json: @task, status: :created, location: @tasks }
+        puts('New Object')
       else
-        format.html { render :new }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
+        format.html { render :show }
+        format.json { render json: @tasks.errors, status: :unprocessable_entity }
+        puts('Somthing went wrong')
       end
-  		end
+
+    end
 
 	end
 
 	def update
 		@task = Task.find(params[:id])
 
-		if @note.update(note_params)
-        	format.html { redirect_to @task, notice: 'Note was successfully updated.' }
-        	format.json { render :show, status: :ok, location: @task }
+		if @task.update(task_params)
+        	format.html { redirect_to tasks_path, notice: 'Note was successfully updated.' }
+        	format.json { render :show, status: :ok, location: @tasks }
 		else
 			format.html { render :edit }
-        	format.json { render json: @task.errors, status: :unprocessable_entity }
+        	format.json { render json: @tasks.errors, status: :unprocessable_entity }
 		end
 	end
 
